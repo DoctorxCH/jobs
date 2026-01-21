3d1b4699976b49ed7b26d54dd58b2c4366868bb1
admin@kurka.ch <admin@kurka.ch@ssh.hostcreators.sk>
2026-01-21 01:35:55 +0100

    fix: allow user company fields + add relations

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
@@ -17,6 +19,10 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+
+        // company binding
+        'company_id',
+        'is_company_owner',
     ];
 
     protected $hidden = [
@@ -29,6 +35,33 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+
+            'company_id' => 'integer',
+            'is_company_owner' => 'boolean',
         ];
+    }
+
+    /**
+     * Primary company (where the user is currently operating from).
+     */
+    public function company(): BelongsTo
+    {
+        return $this->belongsTo(Company::class);
+    }
+
+    /**
+     * Companies the user is a member of (pivot: company_user).
+     */
+    public function companies(): BelongsToMany
+    {
+        return $this->belongsToMany(Company::class)
+            ->using(CompanyUser::class)
+            ->withPivot([
+                'role',
+                'status',
+                'invited_at',
+                'accepted_at',
+            ])
+            ->withTimestamps();
     }
 }
