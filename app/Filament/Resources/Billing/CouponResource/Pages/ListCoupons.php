5b2b566b1a9b35a60c07058d92ca30c50a5cb6b1
admin@kurka.ch <admin@kurka.ch@ssh.hostcreators.sk>
2026-01-28 10:42:38 +0100

    Fix billing list pages: restore Filament list view + add Create header actions

--- app/Filament/Resources/Billing/CouponResource/Pages/ListCoupons.php
+++ app/Filament/Resources/Billing/CouponResource/Pages/ListCoupons.php
@@ -3,9 +3,17 @@
 namespace App\Filament\Resources\Billing\CouponResource\Pages;
 
 use App\Filament\Resources\Billing\CouponResource;
+use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 
 class ListCoupons extends ListRecords
 {
     protected static string $resource = CouponResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
 }
--- app/Filament/Resources/Billing/ProductPriceResource/Pages/ListProductPrices.php
+++ app/Filament/Resources/Billing/ProductPriceResource/Pages/ListProductPrices.php
@@ -3,9 +3,17 @@
 namespace App\Filament\Resources\Billing\ProductPriceResource\Pages;
 
 use App\Filament\Resources\Billing\ProductPriceResource;
+use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 
 class ListProductPrices extends ListRecords
 {
     protected static string $resource = ProductPriceResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
 }
