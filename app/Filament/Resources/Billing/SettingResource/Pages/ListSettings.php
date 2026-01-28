b79a7644c1b260589bf187a941f0be80c528e0d1
DoctorxCH <63975987+DoctorxCH@users.noreply.github.com>
2026-01-28 10:45:23 +0100

    Add create actions to billing list pages

--- app/Filament/Resources/Billing/SettingResource/Pages/ListSettings.php
+++ app/Filament/Resources/Billing/SettingResource/Pages/ListSettings.php
@@ -3,9 +3,17 @@
 namespace App\Filament\Resources\Billing\SettingResource\Pages;
 
 use App\Filament\Resources\Billing\SettingResource;
+use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 
 class ListSettings extends ListRecords
 {
     protected static string $resource = SettingResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
 }
